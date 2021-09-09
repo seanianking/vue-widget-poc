39445998280aa5fe15af836bd35fde3e3cf72c23
Sean King <seanianking@gmail.com>
2021-09-09 14:57:29 -0600

    transaction showing at route

---
+++ src/components/NormalTransactions.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-card>
+      <i class="far fa-5x fa-lightbulb"></i>
+      <p style="font-size: 30px; font-weight: bold">
+        {{ results[0].transfers_count }} Energy Transfers
+      </p>
+    </v-card>
+  </div>
+</template>
+
+<script>
+// let etherkey = process.env.ETHERSCAN_API_KEY;
+// var info;
+export default {
+  name: "NormalTransactions",
+
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    async getData() {
+      try {
+        let response = await fetch(
+          `https://api.bloxy.info/token/token_stat?token=0xb2089a7069861c8d90c8da3aacab8e9188c0c531&key=ACChTwr9Y0PX0&format=structure`
+        );
+        this.results = await response.json();
+        console.log(this.results);
+        console.log(this.results[0].transfers_count + " transfers count");
+        console.log(this.results[0].holders_count + " holders count");
+        console.log(this.results[0].circulating_supply + " circulating supply");
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  created() {
+    this.getData();
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
+import App from './App.vue';
+import NormalTransactions from './components/NormalTransactions.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import vuetify from './plugins/vuetify'
+
+Vue.use(VueRouter);
 Vue.config.productionTip = false
+
+const routes = [
+  {path: '/transactions', component: NormalTransactions},
+  {path: '/', component: HelloWorld},
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+
+})
 
 new Vue({
   render: h => h(App),
+  vuetify,
+  router
 }).$mount('#app')
