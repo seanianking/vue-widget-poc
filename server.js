3e27e2962d5a66424ba05b6ff442c04f36a3a7a1
Sean King <seanianking@gmail.com>
2021-09-14 12:25:36 -0600

    tinkering for heroku deploy

--- server.js
+++ server.js
@@ -3,5 +3,7 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.listen(port);
+
+const PORT = process.env.PORT || 80;
+app.set('port', PORT);
+app.listen(PORT);
