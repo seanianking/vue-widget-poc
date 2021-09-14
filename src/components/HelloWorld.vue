c287727b28576d165ffcc66c1d41e94ba1ee5619
Sean King <seanianking@gmail.com>
2021-09-14 14:30:31 -0600

    removing superfluous buttons

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,8 +9,6 @@
           height="200"
         />
       </v-col>
-<button><a href="/transactions">TRANSACTIONS</a></button>
-<button><a href="/donut">DONUT CHART</a></button>
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
           Welcome to Vuetify
